1531189d52ffd2be740c6ed5255521f3d96fae5c
Eduard Ibatullin <168522996+EduardIbatullin@users.noreply.github.com>
2024-11-17 15:22:01 +0500

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,5 @@ load_dotenv()
 
 class Config:
     TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN', 'BOT_TOKEN')
-    WEATHER_API_KEY = os.getenv('WEATHER_API_KEY', 'WEATHER_API_KEY')
     EXCHANGE_API_KEY = os.getenv('EXCHANGE_API_KEY', 'EXCHANGE_API_KEY')
     X_RAPID_API_KEY = os.getenv('X_RAPID_API_KEY', 'X_RAPID_API_KEY')
